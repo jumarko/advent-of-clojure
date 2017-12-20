4a50d61aeee6d5c593fe5657d9e06a9a07d4843b
Juraj Martinka <jumarko@gmail.com>
2017-12-20 10:18:14 +0100

    Day 2 completed (part II finished).

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jumarko/advent-of-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot advent-of-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/advent_of_clojure/2017/02.clj
+++ src/advent_of_clojure/2017/02.clj
@@ -26,7 +26,26 @@
 
   Part II:
   ------------------------------------------------------------------------------
-  ")
+  \"Great work; looks like we're on the right track after all. Here's a star for your effort.\"
+  However, the program seems a little worried. Can programs be worried?
+  \"Based on what we're seeing, it looks like all the User wanted is some information
+  about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped
+  for that kind of calculation - most of us specialize in bitwise operations.\"
+
+  It sounds like the goal is to find the only two numbers in each row where one evenly divides the other
+  - that is, where the result of the division operation is a whole number.
+  They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+  For example, given the following spreadsheet:
+    5 9 2 8
+    9 4 7 3
+    3 8 6 5
+  In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+  In the second row, the two numbers are 9 and 3; the result is 3.
+  In the third row, the result is 2.
+  In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+  What is the sum of each row's result in your puzzle input?")
 
 
 (defn- read-input
@@ -59,3 +78,52 @@
 ;;=> 18
 (checksum input-rows)
 ;;=> 51833
+
+
+;;; PART II:
+;;;
+
+(defn- find-factors [n candidate-factors]
+  (some
+   #(cond
+      (zero? (rem n %)) [n %]
+      (zero? (rem % n)) [% n])
+   candidate-factors))
+
+(find-factors 9 [7 4 3])
+;;=> [9 3]
+(find-factors 3 [7 4 9])
+;;=> [9 3]
+
+(defn- find-divisibles
+  "Returns a pair (vector) of two numbers in given sequence which are evenly divisible.
+  The greater number is returned as the first element of vector, the smaller number as the second element."
+  [numbers]
+  (loop [num (first numbers)
+         others (rest numbers)]
+    (when (and num others)
+      (if-let [factors (find-factors num others)]
+        factors
+        (recur (first others) (rest others))))))
+
+(find-divisibles [5 9 2 8])
+;;=> [8 2]
+(find-divisibles [9 4 7 3])
+;;=> [9 3]
+(find-divisibles [3 8 6 5])
+;;=> [6 3]
+
+(defn- checksum-2
+  "Computes the checksum of all given rows represented as a sequence of number sequences."
+  [spreadsheet-rows]
+  (apply +
+         (for [row spreadsheet-rows]
+           (let [[x y] (find-divisibles row)]
+             (/ x y)))))
+
+(checksum-2 [[5 9 2 8]
+           [9 4 7 3]
+           [3 8 6 5]])
+;;=> 9
+
+(checksum-2 input-rows)
